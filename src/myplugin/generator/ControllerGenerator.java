2ec3348944f5133d5c00db38aa664b38bbe48a86
Teodora Rajnovic <rajnovicteodora@gmail.com>
2023-11-11 10:31:42 +0100

    Create action generator

---
+++ src/myplugin/generator/ControllerGenerator.java
@@ -0,0 +1,15 @@
+package myplugin.generator;
+
+import myplugin.generator.options.GeneratorOptions;
+
+public class ControllerGenerator extends BackendGenerator {
+
+	public ControllerGenerator(GeneratorOptions generatorOptions) {
+		super(generatorOptions);
+	}
+
+	@Override
+	public void generate() {
+		super.generate();
+	}
+}
