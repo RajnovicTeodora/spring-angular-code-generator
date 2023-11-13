907292e14d2daf718e63d8d70693cb243bab5214
oljamirilovic <69585255+oljamirilovic@users.noreply.github.com>
2023-11-13 18:26:49 +0100

    MyPlugin implemented

--- src/myplugin/MyPlugin.java
+++ src/myplugin/MyPlugin.java
@@ -1,54 +1,88 @@
 package myplugin;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 import javax.swing.JOptionPane;
 
+import myplugin.generator.ControllerGenerator;
+import myplugin.generator.EJBGenerator;
 import myplugin.generator.options.GeneratorOptions;
 import myplugin.generator.options.ProjectOptions;
-
 
 import com.nomagic.actions.NMAction;
 import com.nomagic.magicdraw.actions.ActionsConfiguratorsManager;
 
 /** MagicDraw plugin that performes code generation */
 public class MyPlugin extends com.nomagic.magicdraw.plugins.Plugin {
-	
-	String pluginDir = null; 
-	
+
+	String pluginDir = null;
+
+	private static final String BACKEND_FILEPATH = "src//main//java//uns//ac//rs//mbrs//";
+
 	public void init() {
-		JOptionPane.showMessageDialog( null, "My Plugin init");
+		JOptionPane.showMessageDialog(null, "My Plugin init");
 		
 		pluginDir = getDescriptor().getPluginDirectory().getPath();
-		
-		// Creating submenu in the MagicDraw main menu 	
-		ActionsConfiguratorsManager manager = ActionsConfiguratorsManager.getInstance();		
+
+		// Creating submenu in the MagicDraw main menu
+		ActionsConfiguratorsManager manager = ActionsConfiguratorsManager.getInstance();
 		manager.addMainMenuConfigurator(new MainMenuConfigurator(getSubmenuActions()));
-		
-		/** @Todo: load project options (@see myplugin.generator.options.ProjectOptions) from 
-		 * ProjectOptions.xml and take ejb generator options */
-		
-		//for test purpose only:
-		GeneratorOptions ejbOptions = new GeneratorOptions("c:/temp", "ejbclass", "templates", "{0}.java", true, "ejb"); 				
-		ProjectOptions.getProjectOptions().getGeneratorOptions().put("EJBGenerator", ejbOptions);
-				
-		ejbOptions.setTemplateDir(pluginDir + File.separator + ejbOptions.getTemplateDir()); //apsolutna putanja
+
+		generateOption("ControllerGenerator", "controller", BACKEND_FILEPATH + "controllers", "{0}.java");
+
+		/**
+		 * @Todo: load project options (@see myplugin.generator.options.ProjectOptions)
+		 *        from ProjectOptions.xml and take ejb generator options
+		 */
 	}
 
-	private NMAction[] getSubmenuActions()
-	{
-	   return new NMAction[]{
-			new GenerateAction("Generate"),
-	   };
+	private NMAction[] getSubmenuActions() {
+		return new NMAction[] { new GenerateAction("Generate"), };
 	}
-	
+
 	public boolean close() {
 		return true;
 	}
-	
-	public boolean isSupported() {				
+
+	public boolean isSupported() {
 		return true;
 	}
+
+	private void generateOption(String generatorName, String templateName, String filePath, String fileType) {
+		pluginDir = getDescriptor().getPluginDirectory().getPath();
+		String output_path = getOutputPath();
+		GeneratorOptions option = new GeneratorOptions(output_path, templateName, "templates", fileType, true,
+				filePath);
+		option.setTemplateDir(pluginDir + File.separator + option.getTemplateDir());
+		ProjectOptions.getProjectOptions().getGeneratorOptions().put(generatorName, option);
+	}
+
+	private String getOutputPath() {
+		String output_path = "";
+		Properties prop = new Properties();
+		InputStream input = null;
+		try {
+			input = new FileInputStream("resources/ProjectOptions.xml");
+			// load a properties file
+			prop.load(input);
+			// get the property value and print it out
+			output_path = prop.getProperty("OUTPUT_PATH");
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return output_path;
+	}
+
 }
-
-
